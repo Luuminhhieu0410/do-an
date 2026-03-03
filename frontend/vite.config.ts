da9c8762c645be5fdd04307ecb0f2690920b4349
hieulm <luu7940@gmail.com>
2026-03-03 22:38:58 +0700

    minor change

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+const HOST = "localhost";
+const BE_PORT = 3000;
+const FE_PORT = 5173;
+const proxyOptions = {
+  target: `http://${HOST}:${BE_PORT}`,
+  changeOrigin: false,
+  secure: true,
+  ws: false,
+};
+const proxyConfig = {
+  "^/api(/|(\\?.*)?$)": proxyOptions,
+};
+const serverConfig = {
+  host: HOST, 
+  port: FE_PORT,
+  hmr: true,
+  proxy: proxyConfig,
+  allowedHosts: true,
+};
 export default defineConfig({
   plugins: [react()],
-})
+  server: serverConfig,
+});
